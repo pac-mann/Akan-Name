4c0ac8b7ad51710a6edfeec81cc12535e74baf5c
Kevin Odhiambo Onyango <KOnyango@safaricom.co.ke>
2022-10-15 09:58:30 +0300

    updating business logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const MALE_NAMES = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const FEMALE_NAMES = ["Akosua", "Adwoa","Abenaa","Akua","Yaa","Kofi","Kwame"];
+const DAY_OF_THE_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
 function getUserInputAndCalculateDay() {
 
     var year = prompt("Enter Year: ");
@@ -8,4 +13,27 @@ function getUserInputAndCalculateDay() {
 
     var dayOfTheWeek = math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7)
     return dayOfTheWeek
+}
+
+function getGender () {
+
+    var gender = prompt ("Enter your gender: ");
+    return gender; 
+}
+
+function main(){
+
+    var day = getUserInputAndCalculateDay();
+    var gender = getGender ();
+    var AkanName = null;
+
+    if (gender.toLowerCase().startsWith("f")){
+
+        akanName = FEMALE_NAMES[day];
+    } else {
+
+        akanName= MALE_NAMES[day]
+    }
+
+    console.log(akanName);
 }
